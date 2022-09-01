f290d8f8a1c847dac0d14fd097f0ab7fef96ec27
Injoon Hwang <injoon0617@gmail.com>
2022-09-02 03:45:14 +0900

    :sparkles: (container) add Center

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,10 +2,24 @@ import React from "react";
 
 import styles from "./container.module.scss";
 
-const Stack: React.FC<{ children: React.ReactNode; gap?: string }> = ({
-  children,
-  gap,
-}) => {
+const Center: React.FC<{
+  children: React.ReactNode;
+  column?: boolean;
+}> = ({ children, column }) => {
+  return (
+    <div
+      className={styles.center}
+      style={column ? { flexDirection: "column" } : undefined}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Stack: React.FC<{
+  children: React.ReactNode;
+  gap?: string;
+}> = ({ children, gap }) => {
   return (
     <div className={styles.stack} style={{ gap }}>
       {children}
@@ -14,5 +28,6 @@ const Stack: React.FC<{ children: React.ReactNode; gap?: string }> = ({
 };
 
 export default {
+  center: Center,
   stack: Stack,
 };
