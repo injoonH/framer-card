452afedbf6d30367711f0fa6b3477965c0d673a9
Injoon Hwang <injoon0617@gmail.com>
2022-09-02 01:39:07 +0900

    :sparkles: (Text) add Paragraph

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import styles from "./text.module.scss";
 
+const Paragraph: React.FC<{ children: string }> = ({ children }) => {
+  return <p className={styles.paragraph}>{children}</p>;
+};
+
 const Subtitle: React.FC<{ children: string }> = ({ children }) => {
   return <h3 className={styles.subtitle}>{children}</h3>;
 };
 
 export default {
+  paragraph: Paragraph,
   subtitle: Subtitle,
 };
